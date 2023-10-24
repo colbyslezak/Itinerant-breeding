3644a504af79dd3aec6a761b75a9b4a8340521b4
Colby R. Slezak <colby_slezak@uri.edu>
2023-10-24 13:01:20 -0400

    Update 2023_Proportionplotsformigrationnestingfinalanalysisthatshowproportionofnestattemptsforpub.R

--- 2023_Proportionplotsformigrationnestingfinalanalysisthatshowproportionofnestattemptsforpub.R
+++ 2023_Proportionplotsformigrationnestingfinalanalysisthatshowproportionofnestattemptsforpub.R
@@ -42,7 +42,7 @@ library(grid)
 library(ggpubr)
 
 #login to movebank, this gives me full access to the whole dataset
-login <- movebankLogin(username = "colby_slezak", password="Mamajama12!")
+login <- movebankLogin(username = "colby_slezak", password="requestaccessfromauthor")
 amwo_data <- getMovebankData(study = "American Woodcock Migration Ecology in Eastern North America", 
                              login = login, 
                              removeDuplicatedTimestamps=TRUE)
